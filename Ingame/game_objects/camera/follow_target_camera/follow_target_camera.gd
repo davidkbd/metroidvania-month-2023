86fdb061edf0528edcfd6bc52cba276f3ad98a29
davidkbd <thrash.virgilio@gmail.com>
2023-02-16 20:25:19 +0100

    camara menos brusca al cambiar de hook

--- Ingame/game_objects/camera/follow_target_camera/follow_target_camera.gd
+++ Ingame/game_objects/camera/follow_target_camera/follow_target_camera.gd
@@ -2,24 +2,13 @@ extends Camera2D
 class_name FollowTargetCamera
 
 @export var offset_position : Vector2 = Vector2.UP * 24.0
+@export var distance_factor : float = .25
 
 @onready var target : Node2D = get_tree().get_first_node_in_group("PLAYER")
-
-var requested_target : Node2D
 
 var target_position  : Vector2
 var distance         : float
 var delta_speed      : float
-
-func request(_target : Node2D) -> void:
-	requested_target = _target
-	if target == null or not target is CameraHook:
-		set_target(_target)
-
-func unrequest(_target : Node2D) -> bool:
-	if _target == requested_target: return false
-	target = requested_target
-	return true
 
 func set_target(_target : Node2D) -> void:
 	target = _target
@@ -28,8 +17,8 @@ func teleport() -> void:
 	target_position = target.global_position
 
 func _process(_delta : float) -> void:
-	distance = clamp(target_position.distance_to(target.global_position), 1.0, 150.0) / 5.0
-	delta_speed = _delta * distance
+	distance = clamp(target_position.distance_squared_to(target.global_position), 1.0, 150.0) / 5.0
+	delta_speed = _delta * distance * distance_factor
 	target_position.x = lerp(target_position.x, target.global_position.x, delta_speed)
 	target_position.y = lerp(target_position.y, target.global_position.y, delta_speed)
 	global_position = round(target_position + offset_position)
