458c628ef4a26b85ad30b84fa8eb988bc4f8ec0f
davidkbd <thrash.virgilio@gmail.com>
2023-02-16 19:25:58 +0100

    Correccion posicion del area de camara hook

--- Ingame/game_objects/follow_target_camera/camera_hook/camera_hook.gd
+++ Ingame/game_objects/follow_target_camera/camera_hook/camera_hook.gd
@@ -16,11 +16,11 @@ func _ready():
 	call_deferred("configure_hook")
 
 func configure_hook():
-	var col: CollisionShape2D = get_node("CollisionShape2D")
-	var rect = col.shape.get_rect()
+	var col  : CollisionShape2D = get_node("CollisionShape2D")
+	var rect : Rect2 = col.shape.get_rect()
 	rect.size.x -= get_viewport().size.x
 	rect.size.y -= get_viewport().size.y
-	rect.position.x += rect.size.x / 2
-	rect.position.y += rect.size.y / 2
-	rect.position += global_position
+	rect.position += col.global_position
+	rect.position.x += get_viewport().size.x / 2
+	rect.position.y += get_viewport().size.y / 2
 	hook.set_rect(rect)
