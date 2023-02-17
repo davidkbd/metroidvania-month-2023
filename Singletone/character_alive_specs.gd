4edd5e5be03f9c0c0507deaeec04ede3cad2a473
Jon Huarte <jonnydrumz@gmail.com>
2023-02-17 18:16:38 +0100

    Aumento velocidad salto player

--- Singletone/character_alive_specs.gd
+++ Singletone/character_alive_specs.gd
@@ -8,9 +8,9 @@ static func get_default_specs() -> Dictionary:
 "air_acceleration":    24.0,
 "air_deceleration":    64.0,
 "gravity":             ProjectSettings.get_setting("physics/2d/default_gravity"),
-"jump_impulse":        -ProjectSettings.get_setting("physics/2d/default_gravity") * .25,
+"jump_impulse":        -ProjectSettings.get_setting("physics/2d/default_gravity") * .27,
 "doublejump_impulse":  -ProjectSettings.get_setting("physics/2d/default_gravity") * .25,
-"damage_impulse":      -ProjectSettings.get_setting("physics/2d/default_gravity") * .25,
+"damage_impulse":      -ProjectSettings.get_setting("physics/2d/default_gravity") * .15,
 "damage_deceleration": 20.0,
 "max_up_speed":        -10000.0,
 "max_down_speed":      700.0
