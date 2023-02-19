6b5177d6a9a0858494e0eb9edf21b8894d5e9647
davidkbd <thrash.virgilio@gmail.com>
2023-02-19 01:07:43 +0100

    Cancelo movimiento de la camara si la distancia a recorrer es menos de 2 pixels

--- Ingame/game_objects/camera/follow_target_camera/follow_target_camera.gd
+++ Ingame/game_objects/camera/follow_target_camera/follow_target_camera.gd
@@ -19,9 +19,12 @@ func teleport() -> void:
 
 func _process(_delta : float) -> void:
 	distance = clamp(target_position.distance_squared_to(target.global_position), 1.0, 150.0) / 5.0
+	if distance < 2.0: return
+
 	delta_speed = _delta * distance * distance_factor
 	target_position.x = lerp(target_position.x, target.global_position.x, delta_speed)
 	target_position.y = lerp(target_position.y, target.global_position.y, delta_speed)
+	
 	global_position = round(target_position + offset_position)
 
 func _ready() -> void:
