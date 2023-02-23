187c7a26180bb2f95b01db99fcb0ba4b115f9d08
davidkbd <thrash.virgilio@gmail.com>
2023-02-23 14:26:33 +0100

    Cambio linear_db por log, en las opciones de sonido

--- Menu/options_menu/options_menu.gd
+++ Menu/options_menu/options_menu.gd
@@ -7,13 +7,15 @@ extends Control
 func _on_sfx_volume_value_changed(value : float):
 	if _silent: return
 	get_tree().call_group("MENU_SFX", "play_slider")
-	AudioServer.set_bus_volume_db(1, linear_to_db(value))
+#	AudioServer.set_bus_volume_db(1, linear_to_db(value))
+	AudioServer.set_bus_volume_db(1, log(value) * 20.0)
 	Settings.save_sfx_volume()
 
 func _on_music_volume_value_changed(value):
 	if _silent: return
 	get_tree().call_group("MENU_SFX", "play_slider")
-	AudioServer.set_bus_volume_db(2, linear_to_db(value))
+#	AudioServer.set_bus_volume_db(2, linear_to_db(value))
+	AudioServer.set_bus_volume_db(2, log(value) * 20.0)
 	Settings.save_music_volume()
 
 func _on_controls_value_changed(value):
