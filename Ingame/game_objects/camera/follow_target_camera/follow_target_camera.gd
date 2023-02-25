4bdf05f78b38d6ead489b646509c03230887c771
davidkbd <thrash.virgilio@gmail.com>
2023-02-25 08:20:30 +0100

    Elimino movimiento suave de camara despues de producirse un fade-out/fade-in atravesando un autoadvance

--- Ingame/game_objects/camera/follow_target_camera/follow_target_camera.gd
+++ Ingame/game_objects/camera/follow_target_camera/follow_target_camera.gd
@@ -9,6 +9,13 @@ class_name FollowTargetCamera
 var target_position  : Vector2
 var distance         : float
 var delta_speed      : float
+var fading           : bool
+
+func player_listener_on_autoadvance_exited(area : AutoadvanceArea) -> void:
+	if area.fade_out_in:
+		fading = true
+		await create_tween().tween_interval(.25).finished
+		fading = false
 
 func set_target(_target : Node2D) -> void:
 	target = _target
@@ -18,13 +25,16 @@ func teleport() -> void:
 	global_position = target_position
 
 func _process(_delta : float) -> void:
-	distance = clamp(target_position.distance_squared_to(target.global_position), 1.0, 150.0) / 5.0
-	if distance < 2.0: return
+	if fading:
+		target_position = target.global_position
+	else:
+		distance = clamp(target_position.distance_squared_to(target.global_position), 1.0, 150.0) / 5.0
+		if distance < 2.0: return
 
-	delta_speed = _delta * distance * distance_factor
-	target_position.x = lerp(target_position.x, target.global_position.x, delta_speed)
-	target_position.y = lerp(target_position.y, target.global_position.y, delta_speed)
-	
+		delta_speed = _delta * distance * distance_factor
+		target_position.x = lerp(target_position.x, target.global_position.x, delta_speed)
+		target_position.y = lerp(target_position.y, target.global_position.y, delta_speed)
+		
 	global_position = round(target_position + offset_position)
 
 func _ready() -> void:
