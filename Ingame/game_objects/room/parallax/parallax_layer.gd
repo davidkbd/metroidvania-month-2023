c06646f101f91a15e2eaade5bcd766a934cf2c9e
davidkbd <thrash.virgilio@gmail.com>
2023-02-25 10:18:42 +0100

    Particulas

--- Ingame/game_objects/room/parallax/parallax_layer.gd
+++ Ingame/game_objects/room/parallax/parallax_layer.gd
@@ -2,4 +2,7 @@ extends ParallaxLayer
 
 func room_listener_on_activated(room : Room) -> void:
 	if room.parallax_name == "": return
-	visible = name == room.parallax_name
+	visible = name.begins_with(room.parallax_name)
+	for child in get_children():
+		if child is CPUParticles2D or child is GPUParticles2D:
+			child.emitting = visible
