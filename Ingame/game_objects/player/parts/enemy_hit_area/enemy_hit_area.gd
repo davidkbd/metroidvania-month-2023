4d9da4438750da61fa107ee03429c34061679781
Jon Huarte <jonnydrumz@gmail.com>
2023-02-27 20:51:55 +0100

    Particulas romper pared

--- Ingame/game_objects/player/parts/enemy_hit_area/enemy_hit_area.gd
+++ Ingame/game_objects/player/parts/enemy_hit_area/enemy_hit_area.gd
@@ -9,3 +9,4 @@ func _on_area_entered(area : Area2D):
 	var diff = area.global_position.x - player.global_position.x
 	player.velocity.x = -player.specs.basic_attack_feedback_impulse * sign(diff)
 	player.hitted_enemy = area.get_parent()
+
