6e0fca1fb7ea6506e2ff9a047c1fb0377686f430
davidkbd <thrash.virgilio@gmail.com>
2023-02-27 11:01:12 +0100

    Componente de vidas para hud

---
+++ Ingame/layers/hud/player_hud/lifes.gd
@@ -0,0 +1,47 @@
+@tool
+extends Node2D
+
+@export_group("Data")
+
+@export var value : float = 3.0 :
+	get:
+		return value
+	set(_value):
+		if value == _value: return
+		value = clamp(_value, .0, max_lifes)
+		queue_redraw()
+
+@export var max_lifes : int = 5 :
+	get:
+		return max_lifes
+	set(_value):
+		if max_lifes == _value: return
+		max_lifes = _value
+		queue_redraw()
+
+@export_group("Graphics")
+
+@export var lifes_texture : Texture2D :
+	get:
+		return lifes_texture
+	set(_texture):
+		if lifes_texture == _texture: return
+		lifes_texture = _texture
+		queue_redraw()
+
+func _draw() -> void:
+	if lifes_texture == null: return
+	var lifes   = int(floor(value))
+	var decimas = int(floor(value * 10)) % 10
+	var i : int = 0
+	while i <= lifes and i < max_lifes:
+		_draw_life(10 if i < lifes else decimas, i)
+		i += 1
+	while i < max_lifes:
+		_draw_life(.0, i)
+		i += 1
+
+func _draw_life(_value : int, _x : int) -> void:
+	var rect_src : Rect2 = Rect2(_value * 26, 0, 26, 26)
+	var rect_pos : Rect2 = Rect2(_x * 26, 0, 26, 26)
+	draw_texture_rect_region(lifes_texture, rect_pos, rect_src, Color.WHITE)
