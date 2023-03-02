f6170f80c0d45b0248a851ed958d8a737b387e93
Jon Huarte <jonnydrumz@gmail.com>
2023-03-02 01:40:47 +0100

    Rata se mueve aleatoriamente

--- Ingame/game_objects/items/rat_health/rat_health.gd
+++ Ingame/game_objects/items/rat_health/rat_health.gd
@@ -3,12 +3,14 @@ extends Area2D
 class_name RatHealth
 
 @export  var fall_accel      : float = 800.0
-@export  var speed           : float = 2000.0
+@export  var speed           : float = 7000.0
 
-@onready var sprite          : Sprite2D  = $sprite
-@onready var center_floor_rc : RayCast2D = $center_floor
-@onready var front_floor_rc  : RayCast2D = $front_floor_rc
-@onready var front_wall_rc   : RayCast2D = $front_wall_rc
+@onready var sprite            : Sprite2D  = $sprite
+@onready var center_floor_rc   : RayCast2D = $center_floor
+@onready var front_floor_rc    : RayCast2D = $front_floor_rc
+@onready var front_wall_rc     : RayCast2D = $front_wall_rc
+@onready var animation         : AnimationPlayer = $AnimationPlayer
+@onready var walk_timer        : Timer = $WalkTimer
 
 @onready var space_state = get_world_2d().get_direct_space_state()
 
@@ -18,19 +20,22 @@ class_name RatHealth
 @onready var direction   : int  = 1.0
 
 @onready var velocity : Vector2 = Vector2.ZERO
+@onready var timer
+var walk_timer_flag : bool
 
 func _process(_delta : float) -> void:
 	is_on_floor = center_floor_rc.is_colliding()
 	
-	need_change = is_on_floor \
-			and (not front_floor_rc.is_colliding() or front_wall_rc.is_colliding())
+	need_change = need_change or (
+	is_on_floor and (not front_floor_rc.is_colliding() or front_wall_rc.is_colliding()))
 	
 	if need_change:
 		change_time -= _delta
 		velocity.x = .0
 		if change_time < .0:
 			direction = -direction
-			change_time = .5
+			change_time = 1.0
+			need_change = false
 	else:
 		if is_on_floor:
 			_move(_delta)
@@ -40,6 +45,7 @@ func _process(_delta : float) -> void:
 	
 	scale.x = -direction
 	global_position += velocity * _delta
+	animation.play("walk" if velocity.x else "idle")
 
 func _fall(_delta : float) -> void:
 	velocity.y += _delta * fall_accel
@@ -51,3 +57,13 @@ func _snap_to_floor(_delta) -> void:
 
 func _move(_delta : float) -> void:
 	velocity.x = _delta * direction * speed
+	if (walk_timer_flag == false):
+		need_change = true
+		start_walk_timer()
+
+func _on_walk_timer_timeout():
+	walk_timer_flag = false
+
+func start_walk_timer():
+	walk_timer_flag = true
+	walk_timer.start(randi_range(3,7))
