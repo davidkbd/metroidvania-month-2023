1203b9968ed6bb8b6b013d9bb7e17610b40af6a7
davidkbd <thrash.virgilio@gmail.com>
2023-03-03 16:07:34 +0100

    Look up/down

--- Ingame/game_objects/camera/camera_hook/camera_hook.gd
+++ Ingame/game_objects/camera/camera_hook/camera_hook.gd
@@ -2,26 +2,37 @@ extends Node2D
 
 class_name CameraHook
 
-var target : Player
-var rect   : Rect2
+var target  : Player
+var pos_ini : Vector2
+var pos_end : Vector2
+
+var target_offset : Vector2
 
 func enable(_player : Player) -> void:
 	set_physics_process(true)
 	target = _player
 	_update_position()
+	target_offset = Vector2.ZERO
 
 func disable() -> void:
 	set_physics_process(false)
 
-func set_rect(_rect):
-	rect = _rect
+func set_rect(_rect : Rect2):
+	pos_ini = _rect.position
+	pos_end = pos_ini + _rect.size
 
 func _physics_process(_delta : float) -> void:
+	if ControlInput.is_up_pressed():
+		target_offset.y = -156.0
+	elif ControlInput.is_down_pressed():
+		target_offset.y = 192.0
+	else:
+		target_offset.y = .0
 	_update_position()
 
 func _update_position() -> void:
-	global_position.x = clamp(target.global_position.x, rect.position.x, rect.position.x + rect.size.x)
-	global_position.y = clamp(target.global_position.y, rect.position.y, rect.position.y + rect.size.y)
+	global_position.x = clamp(target.global_position.x,                   pos_ini.x, pos_end.x)
+	global_position.y = clamp(target.global_position.y + target_offset.y, pos_ini.y, pos_end.y)
 
 func _ready() -> void:
 	set_physics_process(false)
