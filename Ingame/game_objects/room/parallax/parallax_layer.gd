4a642f8766250dcd316905f6791516418ab2e4f6
davidkbd <thrash.virgilio@gmail.com>
2023-03-06 20:46:56 +0100

    Activacion y desactivacion de parallax

--- Ingame/game_objects/room/parallax/parallax_layer.gd
+++ Ingame/game_objects/room/parallax/parallax_layer.gd
@@ -1,8 +1,15 @@
 extends ParallaxLayer
 
+@export var parallax_name : String
+
 func room_listener_on_activated(room : Room) -> void:
-	if room.parallax_name == "": return
-	visible = name.begins_with(room.parallax_name)
+	if room.parallax_name == "": 
+		_show(false)
+	else:
+		_show(parallax_name == room.parallax_name)
+
+func _show(_visible : bool) -> void:
+	visible = _visible
 	for child in get_children():
 		if child is CPUParticles2D or child is GPUParticles2D:
-			child.emitting = visible
+			child.emitting = _visible
