4ca7699ce5ea63a3a21ff86bd784d108cd451848
davidkbd <thrash.virgilio@gmail.com>
2023-03-09 23:53:01 +0100

    Solo las armas y proyectiles te activan la invulnerabilidad temporal

--- Ingame/game_objects/player/parts/enemy_damage_area/enemy_damage_area.gd
+++ Ingame/game_objects/player/parts/enemy_damage_area/enemy_damage_area.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var disabled_seconds : float = .2
+@export var disabled_seconds : float = .5
 
 @onready var default_collision_mask = collision_mask
 @onready var player : Player = get_parent()
@@ -16,8 +16,10 @@ func enable_collision() -> void:
 func _on_area_entered(area : Area2D) -> void:
 	player.damager = area.get_damage_data()
 	get_tree().call_group("PLAYER_LISTENER", "player_listener_on_damaged", player)
-	disable_collision()
-	if disable_area_tween: disable_area_tween.kill()
-	disable_area_tween = create_tween()
-	await disable_area_tween.tween_interval(disabled_seconds).finished
-	enable_collision()
+
+	if player.damager.enable_invulnerability:
+		disable_collision()
+		if disable_area_tween: disable_area_tween.kill()
+		disable_area_tween = create_tween()
+		await disable_area_tween.tween_interval(disabled_seconds).finished
+		enable_collision()
