395880a35ab5e28abf64675900ae214687514e24
davidkbd <thrash.virgilio@gmail.com>
2023-03-10 19:11:28 +0100

    Cualquier golpe te hace invulnerable, PERO, al espadazo se la suda y siempre te da

--- Ingame/game_objects/player/parts/enemy_damage_area/enemy_damage_area.gd
+++ Ingame/game_objects/player/parts/enemy_damage_area/enemy_damage_area.gd
@@ -2,24 +2,22 @@ extends Area2D
 
 @export var disabled_seconds : float = .5
 
-@onready var default_collision_mask = collision_mask
 @onready var player : Player = get_parent()
 
 var disable_area_tween : Tween
 
 func disable_collision() -> void:
-	collision_mask = 0
+	set_collision_mask_value(4, false)
 
 func enable_collision() -> void:
-	collision_mask = default_collision_mask
+	set_collision_mask_value(4, true)
 
 func _on_area_entered(area : Area2D) -> void:
 	player.damager = area.get_damage_data()
 	get_tree().call_group("PLAYER_LISTENER", "player_listener_on_damaged", player)
 
-	if player.damager.enable_invulnerability:
-		disable_collision()
-		if disable_area_tween: disable_area_tween.kill()
-		disable_area_tween = create_tween()
-		await disable_area_tween.tween_interval(disabled_seconds).finished
-		enable_collision()
+	disable_collision()
+	if disable_area_tween: disable_area_tween.kill()
+	disable_area_tween = create_tween()
+	await disable_area_tween.tween_interval(disabled_seconds).finished
+	enable_collision()
