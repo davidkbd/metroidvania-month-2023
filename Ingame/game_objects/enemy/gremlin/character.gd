7283efdade116a2214539b12d0811001bae040be
davidkbd <thrash.virgilio@gmail.com>
2023-03-11 00:25:54 +0100

    Mejoro comportamiento gremlin

--- Ingame/game_objects/enemy/gremlin/character.gd
+++ Ingame/game_objects/enemy/gremlin/character.gd
@@ -8,11 +8,18 @@ extends EnemyCharacterAlive
 
 func set_walk_direction(_direction : float) -> void:
 	walk_direction = _direction
-	sword_collider.position = Vector2.LEFT * (-32.0) * walk_direction
+	sword_collider.position = Vector2.LEFT * (-40.0) * walk_direction
 	attack_foot_particles.position.x = 16.0 * walk_direction
 
-func attack_impulse() -> void:
-	velocity.x = specs.attack_impulse * walk_direction
+func attack_impulse(_impulse_count : int) -> void:
+	if player:
+		set_walk_direction(sign(player.global_position.x - global_position.x))
+
+	if _impulse_count == 1:
+		velocity.x = specs.attack_impulse * walk_direction
+	elif _impulse_count == 2:
+		velocity.x = specs.attack_second_impulse * walk_direction
+		velocity.y = -specs.attack_second_impulse
 	attack_foot_particles.restart()
 	attack_foot_particles.emitting = true
 
