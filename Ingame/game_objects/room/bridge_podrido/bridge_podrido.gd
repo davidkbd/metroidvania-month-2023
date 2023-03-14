e4581870e611b9da43b4b10cd21a90dfddeaa849
davidkbd <thrash.virgilio@gmail.com>
2023-03-14 23:43:06 +0100

    Sonido puente

--- Ingame/game_objects/room/bridge_podrido/bridge_podrido.gd
+++ Ingame/game_objects/room/bridge_podrido/bridge_podrido.gd
@@ -8,3 +8,8 @@ func _break_begin() -> void:
 	super._break_begin()
 	get_tree().call_group("ROOM_LISTENER", "room_listener_on_dramatic_event")
 	destroyed = true
+	$breaking_sfx.play()
+
+func _break() -> void:
+	super._break()
+	$rocks_sfx.play()
