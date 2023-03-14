7f95d0bdb79a31fbab0c1e756789c5cdcfc93498
davidkbd <thrash.virgilio@gmail.com>
2023-03-14 01:20:12 +0100

    Particulas parallax

--- Ingame/game_objects/bridge_podrido/bridge_podrido.gd
+++ Ingame/game_objects/bridge_podrido/bridge_podrido.gd
@@ -5,6 +5,10 @@ func _break_begin() -> void:
 	get_tree().call_group("ROOM_LISTENER", "room_listener_on_dramatic_event")
 	ProgressManager.game_state.bundle.bridge_podrido.destroyed = true
 
+func _restore() -> void:
+	# aqui no llega porque esta en pausa el room
+	queue_free()
+
 func _ready():
 	if ProgressManager.game_state.bundle.bridge_podrido.destroyed:
 		queue_free()
