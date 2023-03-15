24aa2a32f8b13caabe4a9b3ec3558e0331c77e6f
Jon Huarte <jonnydrumz@gmail.com>
2023-03-15 16:53:56 +0100

    El slime te da mas info del salto

--- Ingame/game_objects/npc/slime_04/character.gd
+++ Ingame/game_objects/npc/slime_04/character.gd
@@ -8,7 +8,8 @@ func get_texts() -> Array[String]:
 	$talk_sfx.play()
 	data.last_talk = Time.get_unix_time_from_system()
 	return [
-		"Check that flower with energy from the roots of the underground",
+		"Check this flower with energy from the roots of the underground",
 		"That one lets you stick into a wall and jump to the other direction",
+		"Just jump on a wall to stick to it, and jump again facing the other direction",
 		"It's so funny, give it a try"
 	]
